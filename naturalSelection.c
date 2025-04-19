55caa99ed2d3a88420c55d47e35badca180646ff
lance <atrpwq@proton.me>
2025-04-19 16:30:36 +0000

    Add files via upload

--- naturalSelection.c
+++ naturalSelection.c
@@ -1,122 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 
-extern int bias(int bias, int currentNode);
+extern int  bias(int bias, int currentNode);
 extern void output(int node, int status, int nodeBias);
+extern void outputGenerations(int node, int generations);
+
+typedef struct {
+	int node,
+	    status,
+	    population,
+	    generation;
+} node;
+
+#define alive 1
+#define dead 0
 
 int main(int argc, char **argv){
 	srand(time(NULL));
 
-	int currentBias = rand() % 10 + 1;
+	char *flag = argv[3];
 
-	int firstNodeGeneration = 1,
-	    secondNodeGeneration = 1,
-	    thirdNodeGeneration = 1,
-	    fourthNodeGeneration = 1,
-	    fifthNodeGeneration = 1,
-	    sixthNodeGeneration = 1;
-	
-	int firstNode = 1, 
-	    secondNode = 1, 
-	    thirdNode = 1,
-	    fourthNode = 1,
-	    fifthNode = 1,
-	    sixthNode = 1;
+	int generation = 1;
 
-	int firstNodeBias = rand() % 10,
-	    secondNodeBias = rand() % 10,
-	    thirdNodeBias = rand() % 10,
-	    fourthNodeBias = rand() % 10,
-	    fifthNodeBias = rand() % 10,
-	    sixthNodeBias = rand() % 10;
+	node first  = 	{1, alive, rand() % 10, generation},
+	     second = 	{2, alive, rand() % 10, generation},
+	     third  = 	{3, alive, rand() % 10, generation},
+	     fourth = 	{4, alive, rand() % 10, generation},
+	     fifth  = 	{5, alive, rand() % 10, generation},
+	     sixth  = 	{6, alive, rand() % 10, generation}; 
 
-	if (argv[1]!=NULL)
-		while (1){
-			currentBias = rand() % 10 + 1;
-	
-			int firstNode = 1, 
-			    secondNode = 1, 
-			    thirdNode = 1,
-			    fourthNode = 1,
-			    fifthNode = 1,
-			    sixthNode = 1;		
+	if (argv[1]!=NULL && argv[2] != NULL)
+		while (generation <= atoi(argv[2])){
+			int currentBias = rand() % 10 + 1;
 
-			for (int i = 0; i <= atoi(argv[1]); i++) {
+			first.generation  = 0;
+			second.generation = 0;
+			third.generation  = 0;
+			fourth.generation = 0;
+			fifth.generation  = 0;
+			sixth.generation  = 0;
+
+			first.status  = alive;
+			second.status = alive;
+			third.status  = alive;
+			fourth.status = alive;
+			fifth.status  = alive;
+			sixth.status  = alive;
+
+			for (int i = 1; i <= atoi(argv[1]); i++) {
 
 			system("clear");
+			
+			printf("%d\n\n", generation);
 			printf("%d\n", currentBias);
 			
-			if (firstNode == 0) firstNodeBias = 0;
-			else { 
-				firstNodeBias += rand() % 8; 
-				firstNodeGeneration++;
-			}
-
-			if (secondNode == 0) secondNodeBias = 0;
-			else { 
-				secondNodeBias += rand() % 8;
-				secondNodeGeneration++;
-			}
-
-			if (thirdNode == 0) thirdNodeBias = 0;
-			else { 
-				thirdNodeBias += rand() % 8;
-				thirdNodeGeneration++;
-			}
-
-			if (fourthNode == 0) fourthNodeBias = 0;
-			else { 
-				fourthNodeBias += rand() % 8;
-				fourthNodeGeneration++;
-			}
-
-			if (fifthNode == 0) fifthNodeBias = 0;
+			if (first.status == dead) first.population = 0;
 			else {
-				fifthNodeBias += rand() % 8;
-				fifthNodeGeneration++;
+				first.population += rand() % 5 + 2; 
+				first.generation++;
 			}
 
-			if (sixthNode == 0) sixthNodeBias = 0;
+			if (second.status == dead) second.population = 0;
 			else {
-				sixthNodeBias += rand() % 8;
-				sixthNodeGeneration++;
+				second.population += ceil(currentBias / 4);
+				second.generation++;
 			}
 
-			output(1, firstNode, firstNodeBias);
-			output(2, secondNode, secondNodeBias);
-			output(3, thirdNode, thirdNodeBias);
-			output(4, fourthNode, fourthNodeBias);
-			output(5, fifthNode, fifthNodeBias);
-			output(6, sixthNode, sixthNodeBias);
+			if (third.status == dead) third.population = 0;
+			else {
+				third.population += ceil(currentBias / 3);
+				third.generation++;
+			}
 
-			if (bias(firstNodeBias, currentBias) == 0) 
-				firstNode = 0;
-			if (bias(secondNodeBias, currentBias) == 0) 
-				secondNode = 0;
-			if (bias(thirdNodeBias, currentBias) == 0) 
-				thirdNode = 0;
-			if (bias(fourthNodeBias, currentBias) == 0) 
-				fourthNode = 0;
-			if (bias(fifthNodeBias, currentBias) == 0) 
-				fifthNode = 0;
-			if (bias(sixthNodeBias, currentBias) == 0) 
-				sixthNode = 0;
+			if (fourth.status == dead) fourth.population = 0;
+			else {
+				fourth.population += rand() % 8;
+				fourth.generation++;
+			}
+
+			if (fifth.status == dead) fifth.population = 0;
+			else {
+				fifth.population += floor(currentBias / 5) + 5;
+				fifth.generation++;
+			}
+
+			if (sixth.status == dead) sixth.population = 0;
+			else {
+				sixth.population += ceil((currentBias + 1) / 7);
+				sixth.generation++;
+			}
+
+			output(	first.node , first.status , first.population  );
+			output(	second.node, second.status, second.population );
+			output(	third.node , third.status , third.population  );
+			output(	fourth.node, fourth.status, fourth.population );
+			output( fifth.node , fifth.status , fifth.population  );
+			output(	sixth.node , sixth.status , sixth.population  );
+
+			if (bias(first.population, currentBias ) == 0) first.status  = dead;
+			if (bias(second.population, currentBias) == 0) second.status = dead;
+			if (bias(third.population, currentBias ) == 0) third.status  = dead;
+			if (bias(fourth.population, currentBias) == 0) fourth.status = dead;
+			if (bias(fifth.population, currentBias ) == 0) fifth.status  = dead;
+			if (bias(sixth.population, currentBias ) == 0) sixth.status  = dead;
 
 			currentBias += rand() % 7;
+
+			puts("");
+			outputGenerations(first.node , first.generation );
+			outputGenerations(second.node, second.generation);
+			outputGenerations(third.node , third.generation );
+			outputGenerations(fourth.node, fourth.generation);
+			outputGenerations(fifth.node , fifth.generation );
+			outputGenerations(sixth.node , sixth.generation );
+		
+			}
+	
+	if(flag != NULL && argv[4] != NULL)
+		if(strcmp(flag, "-t") == 0)
+			sleep(atoi(argv[4]));
+	generation++;
 		}
 	
-	printf("\n1: %d\n2: %d\n3: %d\n4: %d\n5: %d\n6: %d\n",
-			firstNodeGeneration,
-			secondNodeGeneration,
-			thirdNodeGeneration,
-			fourthNodeGeneration,
-			fifthNodeGeneration,
-			sixthNodeGeneration);
-	sleep(5);
-		}
-
 	return 0;
 }
 
@@ -128,4 +137,8 @@ int bias(int bias, int currentNode){
 
 void output(int node, int status, int currentBias){
 	printf("[%d]: %d, %d\n", node, status, currentBias);
+}
+
+void outputGenerations(int node, int generations){
+	printf("%d: %d\n", node, generations);
 }
